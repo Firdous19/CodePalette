5964f26f6be664c52144608a80cf4b1ee8fa1e47
Aditya Ghosh <adi27.ghosh@gmail.com>
2023-12-29 00:23:25 +0530

    insertion commit

--- C/LinkedLists/CircularSinglyLl.c
+++ C/LinkedLists/CircularSinglyLl.c
@@ -33,6 +33,38 @@ struct node *insert_at_beg(struct node *tail, int data)
     return tail;
 }
 
+struct node *insert_at_end(struct node *tail, int data)
+{
+    struct node *temp;
+    temp = (struct node*)malloc(sizeof(struct node));
+    temp->info = data;
+    temp->link = NULL;
+
+    temp->link = tail->link;
+    tail->link = temp;
+    tail = tail->link;
+    return tail;
+}
+
+struct node *insert_at_any_pos(struct node *tail, int pos, int data)
+{
+    struct node *p = tail->link;
+    struct node *temp = (struct node*)malloc(sizeof(struct node));
+    temp->info = data;
+    temp->link = NULL;
+
+    while(pos > 1)
+    {
+        p = p->link;
+        pos--;
+    }
+    temp->link = p->link;
+    p->link = temp;
+    if(p == tail)
+        tail = tail->link;
+    return tail;
+}
+
 void traverse(struct node *tail)
 {
     struct node *p;
